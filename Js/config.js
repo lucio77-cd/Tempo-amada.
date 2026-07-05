3df0f9ca93b24f448434d138ce7b5f902a1cdeaa
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-07-05 20:56:01 -0300

    Update config.js

--- Js/config.js
+++ Js/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
   // Chave do OpenWeatherMap (gerar em https://home.openweathermap.org/api_keys)
   // Usa o endpoint gratuito "5 day / 3 hour forecast" (/data/2.5/forecast)
-  owmApiKey: "COLOQUE_SUA_CHAVE_AQUI",
+  owmApiKey: "1347f4c4f81a3f5d6917f93268d250fd",
 
   // Chave do GIPHY (gerar em https://developers.giphy.com)
   giphyApiKey: "COLOQUE_SUA_CHAVE_AQUI",
